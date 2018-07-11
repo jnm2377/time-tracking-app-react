5ff6175b9462f6b53ad94c634b01be27b74d52fd
Josefina Mancilla <josefinamancilla@josefinas-mbp.austin.ibm.com>
2018-07-11 16:27:42 -0500

    created second child ToggleableTimerForm and second great grand child Timer, and rendered app

--- public/js/app.js
+++ public/js/app.js
@@ -38,6 +38,26 @@ class EditableTimerList extends React.Component {
   }
 }
 
+//CHILD 2 ===============================================
+class ToggleableTimerForm extends React.Component {
+  render() {
+    if(this.props.isOpen) {
+      return (
+        <TimerForm/>
+      );
+    } else {
+      return (
+        <div className='ui basic content center aligned segment'>
+          <button className='ui basic button icon'>
+            <i className='plus icon'/>
+          </button>
+        </div>
+      )
+    }
+  }
+}
+
+
 //GRANDCHILD 1 ========================================
 class EditableTimer extends React.Component {
   render() {
@@ -89,3 +109,43 @@ class TimerForm extends React.Component {
     )
   }
 }
+
+//GREAT GRANDCHILD 2 ========================================
+class Timer extends React.Component {
+  render() {
+    const elapsedString = helpers.renderElapsedString(this.props.elapsed);
+    return (
+      <div className='ui centered card'>
+        <div className='content'>
+          <div className='header'>
+            {this.props.title}
+          </div>
+          <div className='meta'>
+            {this.props.project}
+          </div>
+          <div className='center aligned description'>
+            <h2>
+              {elapsedString}
+            </h2>
+          </div>
+          <div className='extra content'>
+            <span className='right floated edit icon'>
+              <i className='edit icon' />
+            </span>
+            <span className='right floated trash icon'>
+              <i className='trash icon' />
+            </span>
+          </div>
+        </div>
+        <div className='ui bottom attached blue basic button'>
+          Start
+        </div>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(
+  <TimersDashboard/>,
+  document.getElementById('content')
+);

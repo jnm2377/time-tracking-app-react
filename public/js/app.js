f19fbef9f8b4c27e4b1e75bf41420933e8a0518d
Josefina Mancilla <josefinamancilla@josefinas-mbp.austin.ibm.com>
2018-07-11 15:26:54 -0500

    created TimersDashboard parent component and child component EditableTimerList

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,37 @@
+class TimersDashboard extends React.Component {
+  render() {
+    return (
+      <div className='ui three column centered grid'>
+        <div className='column'>
+          <EditableTimerList/>
+          <ToggleableTimerForm
+            isOpen={true}
+          />
+        </div>
+      </div>
+    )
+  }
+}
 
+class EditableTimerList extends React.Component {
+  render() {
+    return (
+      <div id='items'>
+        <EditableTimer
+          title='Learn React'
+          project='Web Domination'
+          elapsed='8986300'
+          runningSince={null}
+          editFormOpen={false}
+        />
+        <EditableTimer
+          title='Learn extreme ironing'
+          project='World Domination'
+          elapsed='3247897'
+          runningSince={null}
+          editFormOpen={true}
+        />
+      </div>
+    )
+  }
+}

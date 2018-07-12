9026e3653681e2228ded429a308fc8529beb38e2
Josefina Mancilla <josefinamancilla@josefinas-mbp.austin.ibm.com>
2018-07-12 11:42:37 -0500

    added state to form

--- public/js/app.js
+++ public/js/app.js
@@ -115,6 +115,19 @@ class EditableTimer extends React.Component {
 
 //GREAT GRANDCHILD 1 ========================================
 class TimerForm extends React.Component {
+  state = {
+    title: this.props.title || '',
+    project: this.props.project || ''
+  };
+
+  handleTitleChange = (e) => {
+    this.setState( {title: e.target.value} );
+  };
+
+  handleProjectChange = (e) => {
+    this.setState( {project: e.target.value} );
+  };
+
   render() {
     const submitText = this.props.title ? 'Update' : 'Create';
     return (
@@ -123,11 +136,19 @@ class TimerForm extends React.Component {
           <div className='ui form'>
             <div className='field'>
               <label>Title</label>
-              <input type='text' defaultValue={this.props.title}/>
+              <input
+                type='text'
+                value={this.props.title}
+                onChange={this.handleTitleChange}
+              />
             </div>
             <div className='field'>
               <label>Project</label>
-              <input type='text' defaultValue={this.props.project}/>
+              <input
+                type='text'
+                value={this.props.project}
+                onChange={this.handleProjectChange}
+              />
             </div>
             <div className='ui two bottom attached buttons'>
               <button className='ui basic blue button'>
